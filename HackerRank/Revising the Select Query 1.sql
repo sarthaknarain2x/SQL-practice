4c4aa6e3f9835268639d87e4a0f3f75c9b101c34
Sarthak Narain <sarthaknarain27@gmail.com>
2023-02-03 17:05:43 +0530

    Create Revising the Select Query 1.sql

---
+++ HackerRank/Revising the Select Query 1.sql
@@ -0,0 +1,2 @@
+//Question: Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+SELECT * FROM CITY WHERE population>100000 AND countrycode = 'USA';
