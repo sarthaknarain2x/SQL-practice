03c2fef40085d242c47d4fe3618ef2fb5deaa6d6
Sarthak Narain <sarthaknarain27@gmail.com>
2023-04-14 09:51:00 +0530

    using if and case

--- Leetcode/610 Triangle Judgement.sql
+++ Leetcode/610 Triangle Judgement.sql
@@ -2,3 +2,7 @@ SELECT x, y, z,
 CASE WHEN x+y > z AND x+z > y AND y+z > x THEN 'Yes'
 ELSE 'No'
 END AS triangle FROM Triangle; 
+
+#Using IF
+# SELECT x, y, z, 
+# IF(x+y>z AND y+z>x AND x+z>y, 'Yes', 'No') AS triangle FROM Triangle;
