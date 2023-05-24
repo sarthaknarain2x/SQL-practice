8094e46d3566895805ce41a372509f3101900d34
Sarthak Narain <sarthaknarain27@gmail.com>
2023-05-24 14:02:44 +0530

    Update 1667 Fix Names in a Table.sql

--- Leetcode/1667 Fix Names in a Table.sql
+++ Leetcode/1667 Fix Names in a Table.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT user_id, CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name, LENGTH(name)-1))) AS name FROM Users ORDER BY user_id;
 
--- another approach
+-- another way
 -- SELECT user_id, CONCAT(UPPER(SUBSTRING(name,1,1)), LOWER(SUBSTRING(name, 2))) AS name FROM Users ORDER BY user_id;
